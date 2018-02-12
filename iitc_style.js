24376a469abcb15ff46e7c34cff04f40aa41bafa
melomano <latin.dj.melomano@gmail.com>
2018-02-12 14:00:24 +0900

    Update iitc_style.js

--- iitc_style.js
+++ iitc_style.js
@@ -1,1 +1,3 @@
 document.getElementById('chatcontrols').style.display = 'none';
+document.getElementById('chat').style.display = 'none';
+document.getElementById('chatinput').style.display = 'none';
